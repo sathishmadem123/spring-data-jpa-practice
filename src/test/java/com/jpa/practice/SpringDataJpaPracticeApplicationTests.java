8647cccb16fd64d0b011a04d3b94ce6fb28d67d3
Sathish Madem <sathishmadem123@gmail.com>
2025-08-02 00:16:17 +0530

    Initial Commit

---
+++ src/test/java/com/jpa/practice/SpringDataJpaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
